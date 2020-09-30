cec478c8ba9baf411490953e0a8079fe2e4c8d1b
Satan <patrik__96@hotmail.com>
2020-09-30 21:22:34 +0200

    Started working on actual serverstuff

--- example.cpp
+++ example.cpp
@@ -2,12 +2,17 @@
 int main(void)
 {
     sNet::server s;
-    auto a = s.poll<int>(2);
+    
     s.registerHandle([&](std::string s) 
     {
             // Dostuff
     }, 5);
+    s.send(2, 125, 33, 45.5);
+    auto a = s.poll<int, int, int, double>(2);
+
     std::cout << std::get<0>(a) << std::endl;
-        
+
+    std::cout << std::get<0>(a) * 2 << std::endl;
+
     return 0;
 }
--- sNet/network.hpp
+++ sNet/network.hpp
@@ -7,6 +7,26 @@
 #include <sstream>
 #include <string.h>
 #include <thread>
+#include <numeric>
+#include <chrono>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h> 
+
+
+/*
+    TODO:
+    Too many mutex?
+
+*/
 namespace sNet
 {
 
@@ -46,14 +66,11 @@ namespace sNet
     class server
     {
     private:
-
+        bool isRunning = false;
         mutable std::mutex mutex;
         mutable std::mutex tsend;
         mutable std::mutex remove;
-        /*
-            Race condition med poll och callback. måste använda ett lås
-            för att läsa och skriva. Kanske skriva en wrapper över mappen?
-        */
+
         std::unordered_map<unsigned short, std::string> rawdata;
         std::unordered_map<unsigned short, std::function<void(std::string)>> callbacks;
         std::vector<char*> to_send;
@@ -66,7 +83,42 @@ namespace sNet
             //Som i clearData
         
             std::lock_guard<std::mutex> lock_guard_name(mutex);
-            return rawdata[i];
+            auto raw = rawdata[i];
+            auto size = raw.find("<>");
+            return raw.substr(0, size - 1);
+        }
+        /*
+            Untested af
+
+        */
+        void serverWriteData(int index, char* data)
+        {
+            std::lock_guard<std::mutex> lock_guard_name(mutex);
+            std::string dat(data);
+            std::string work;
+            size_t len = 0;
+            size_t itlen = data.length();
+            while(dat.length() > 0)
+            {
+                
+                for(unsigned i = 0; i < itlen; i++)
+                {
+                    if(dat[i] == '<' && dat[i + 1] == '>' )
+                    {
+                        len = i + 2;
+                        break;
+                    }
+                    work.push_back(dat[i]);
+                }
+                dat.erase(0, len);
+                itlen = dat.length();
+                auto key = atoi(work[0]);
+                rawdata[key] = work;
+                work.clear();
+            }
+
+            // Hitta antalet paket och dela upp dem till rätt ställe
+
         }
         void addToSend(char* data)
         {
@@ -76,39 +128,59 @@ namespace sNet
         void clearData(unsigned index)
         {
             std::lock_guard<std::mutex> lock_guard_name(remove);
-            size_t s = 0;
-            for(unsigned i = 0; i < rawdata[i].size() + 1; i++)
+            /*bool data = rawdata[index].size() > 0;
+            if(auto pos = rawdata[index].find("<>") != std::string::npos)
             {
-                if(rawdata[index][i] == '<' && rawdata[index][i + 1] == '>')
-                {
-                    rawdata[index].erase(0, s + 1);
-                    return;
-                }
+                rawdata[index].erase(0, pos - 1);
+                return;
             }
+            if(data) rawdata[index].erase();*/
             rawdata[index].clear();
-            return;
         }
     public:
         server(const server &other)
         {
             std::lock_guard<std::mutex>(other.mutex);
             std::lock_guard<std::mutex>(other.tsend);
-            std::lock_guard<std::mutex>(other.mutex);
+            std::lock_guard<std::mutex>(other.remove);
         }
         server()
         {
             srand(time(NULL));
-            rawdata[2] = "2 5 2.15";
+        }
+        size_t getWaitingData(unsigned index)
+        {
+            std::lock_guard<std::mutex> lock_guard_name(mutex);
+            auto data = rawdata[index];
+            size_t counter = 0;
+            size_t start = 0;
+            if((start = data.find("<>")) == std::string::npos) return 0;
+            counter++;
+            for(unsigned i = start + 1; i < data.size();)
+            {
+                auto pos = data.find("<>");
+                pos != std::string::npos ? counter++ : i = data.size() + 1;
+                i = pos + 1;
+            }
+            return counter;
         }
         template<protocol s>
-        bool launch()
+        bool launch(unsigned short port, const char* ip = "0.0.0.0", unsigned short tickrate = 64)
         {
+            using namespace std::chrono_literals;
             if constexpr(s == TCP)
             {
                 thr = new std::thread([&]()
                 {
-                    rawdata[5] = "fok";
-                    std::cout << "whatthefuck?" << std::endl;
+                    double sleeprate = 1.0 / (tickrate);
+                   
+                    /*int listenfd = 0, connfd = 0;
+                    struct sockaddr_in serv_addr;
+                    auto listenfd = socket(AF_INET, SOCK_STREAM, 0);*/
+                    while(isRunning)
+                    {
+                        std::this_thread::sleep_for(std::chrono::seconds(sleeprate));
+                    }
                 });
                 thr->detach();
                 
@@ -119,27 +191,40 @@ namespace sNet
             }
             return true;
         }
-        template<typename... Args>
-        void send(unsigned short key, Args... to_send)
+        inline void shutDown()
         {
+            isRunning = false;
+        }
+        template<typename... Args>
+        void send(unsigned key, Args... to_send)
+        {
+            /*
+                TODO:
+                dela upp i chunks om det behövs. 
+                Borde inte vara något problem tbh, 
+                men låt bli att skriva <> om det ska bli längre
+            */
             std::stringstream ss;
-            ss << key;
-            (ss << ... << to_send);
-            (ss << "<>");
+            ss << key << " ";
+            ((ss << to_send << " "), ...);
+            if(sizeof(ss.str()) < 1024)
+            {
+                (ss << "<>");
+            }
             auto data = ss.str();
-
+            std::cout << data << std::endl;
+            addToSend((char*)data.c_str());
+            rawdata[2] = data;
         }
         template <typename... Ts>
         std::tuple<Ts...> poll(short i)
         {
             std::tuple<Ts...> ret;
             //auto size = std::tuple_size<decltype(ret)>::value;
-            //using type = typename std::tuple_element<0, std::tuple<Ts...>>::type;
             if(callbacks.find(i) != callbacks.end() || 
                 getData(i).empty()) return ret;
             std::string parse(getData(i));
             std::istringstream ss(parse);
- //           unsigned counter = 0;
             for_each(ret, [&](auto& item) 
             {
                 ss >> item;
@@ -148,9 +233,9 @@ namespace sNet
             clearData(i);
             return ret;
         }
-        void registerHandle(std::function<void(std::string)> func, short key)
+        inline void registerHandle(std::function<void(std::string)> func, short key)
         {
-            callbacks[key] = func;
+            if(callbacks.find(key) != callbacks.end()) callbacks[key] = func;
         }
         ~server()
         {
